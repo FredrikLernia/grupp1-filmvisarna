929bc2b83bd7e7e52dad531cd4e1df28259dcc92
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-01-23 13:57:36 +0100

    Component for repertoire

--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -3,7 +3,7 @@ class PageContent extends Component {
   constructor(){
     super();
     this.startPage = new StartPage();
-    this.Booking = new Booking();
+    this.booking = new RepertoirePage();
     this.missingPage = new MissingPage();
   }
 
