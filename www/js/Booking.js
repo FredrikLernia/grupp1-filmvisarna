bce15196443f03a90a41d40c747ef7f1987e3fd1
Kim <kim.johnsson@telia.com>
2019-01-23 13:15:55 +0100

    adding one on click

--- www/js/Booking.js
+++ www/js/Booking.js
@@ -3,7 +3,14 @@ class Booking extends Component {
     constructor(){
       super();
       this.addRoute('/boka', 'Boka');
+      this.bookinNum = 0;
+      this.addEvents({
+          'click button' : 'getBookingNum'});
     }
-  
+
+    getBookingNum(){
+        this.bookinNum++
+        console.log(this.bookinNum);
+    }
   
   }
