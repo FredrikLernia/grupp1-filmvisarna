d614c70deca4a6cf5392c5f82f9cdff16bace130
Kim <kim.johnsson@telia.com>
2019-01-23 11:56:23 +0100

    nav- links

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -3,7 +3,11 @@ class NavBar extends Component {
   constructor(){
     super();
     this.navItems = [
-      new NavItem('Start', '/'),
+      new NavItem('Filmvisarna', '/'),
+      new NavItem('Boka', '/boka'),
+      new NavItem('Filmer', '/filmer'),
+      new NavItem('Salonger', '/salonger'),
+      new NavItem('Hitta oss', '/hitta-oss')
     ];
   }
 
