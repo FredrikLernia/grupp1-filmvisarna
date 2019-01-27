eb1a40cc912ff6c39eb73c5fd166b2d16b8b5524
robinasplund <r.asplund@mail.com>
2019-01-27 22:42:05 +0100

    Ngt slags första flummande

--- www/js/ConfirmationPage.js
+++ www/js/ConfirmationPage.js
@@ -1,12 +1,31 @@
 class ConfirmationPage extends Component{
 
-    constructor(){
+    constructor(repetoire){
 
         super();
-        this.addRoute('/jjj','Confirmation Page');
+        this.addRoute('/bokabiljett/konfirmera', 'Boka');
+
+        this.repetoire = {
+            auditorium: "Lilla Salongen",
+            movie: "Vilda Västern 5",
+            date: "05-02-2019",
+            time: "20.00",
+            guests: "42",
+            price: "120.00",
+            seatsTaken: "Yes"
+        };
+        this.confirmBooking();
     }
 
+    async confirmBooking(){
+        //let repetoire = await Repetoire.find(bookingNum);
+        //console.log(repetoire.date);
 
+        //let x = new Auditorium({name:'olle',"seatsPerRow": [3,4]});
+        //console.log(x);
+
+    }
+    
 
 
 }
